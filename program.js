f31390652459d7f20e29ab61bfcfe42946b9e068
Sarah Graves Smith <smith.g.sarah@gmail.com>
2017-06-19 13:25:27 -0400

    Used process.argv to create addition function.

--- program.js
+++ program.js
@@ -1,1 +1,8 @@
-console.log("HELLO WORLD");
+// console.log("HELLO WORLD");
+
+// console.log(process.argv);
+let result = 0;
+process.argv.slice(2).forEach(function(element) {
+  result += Number(element);
+});
+console.log(result);
