e916c0569762d3b963619323c74212040c63ebe1
Sarah Graves Smith <smith.g.sarah@gmail.com>
2017-06-21 12:51:37 -0400

    Refactored function to simply it and resolve the bug. WIP.

--- program.js
+++ program.js
@@ -3,10 +3,10 @@
 
 //BABY STEPS
 // console.log(process.argv);
-let result = 0;
-process.argv.slice(2).forEach(function(element) {
-  result += Number(element);
-});
+// let result = 0;
+// process.argv.slice(2).forEach(function(element) {
+//   result += Number(element);
+// });
 // console.log(result);
 
 //MY FIRST I/O!
@@ -17,11 +17,6 @@ process.argv.slice(2).forEach(function(element) {
 
 //MY FIRST ASYNC I/O!
 let fs = require('fs');
-  let path = require('path');
-  fs.readdir(process.argv[2], function (err, list) {
-    list.forEach(function (file) {
-      if (path.extname(file) === '.' + process.argv[3]) {
-        console.log(file);
-      }
-    });
-  });
+fs.readFile(process.argv[2], function (err, data) {
+  //TODO
+});
